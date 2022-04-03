0910cb483a0d7a74141af28518bcc1765e7d6676
aktarinaakhi <aktarinacsehstu@gmail.com>
2022-04-04 01:03:05 +0600

    string replace or add

--- lamda/rss-feeds.js
+++ lamda/rss-feeds.js
@@ -1,34 +1,40 @@
-const {xml2json} = require("xml-js"),
-      fetch = require('node-fetch');
+const { xml2json } = require("xml-js"),
+    fetch = require('node-fetch');
 
-module.exports.RSSFeeds = async() => {
+module.exports.RSSFeeds = async () => {
 
-    try{
+    try {
 
         const resp = await fetch('https://moneywithmakandg.libsyn.com/rss'),
-              resp_body = await resp.text();
+            resp_body = await resp.text();
         const respJson = await xml2json(resp_body);
 
-        const {elements} = JSON.parse(respJson),
-              rssElements = elements[0].elements,
-              channelElements = rssElements[0].elements;
+        const { elements } = JSON.parse(respJson),
+            rssElements = elements[0].elements,
+            channelElements = rssElements[0].elements;
 
         let audioData = [];
-        
-        // rss channel elements
-        for(let elem of channelElements){
 
-            if(elem.name === "item"){
+        // rss channel elements
+        for (let elem of channelElements) {
+
+            if (elem.name === "item") {
 
                 let obj = {};
 
                 // items from channel elements object
-                for(let items of elem.elements){
+                for (let items of elem.elements) {
 
-                    if(items.name === "title")
-                        obj.title = items.elements[0].text;
+                    if (items.name === "title") {
+                        var a = obj.title;
+                        const b = "isode ";
+                        const position = 2;
+                        a = [a.slice(0, position), b, a.slice(position)].join('');
+                        a = items.elements[0].text;
+                    }
 
-                    if(items.name === "enclosure")
+
+                    if (items.name === "enclosure")
                         obj.url = items.attributes.url;
                 }
 
@@ -37,7 +43,7 @@ module.exports.RSSFeeds = async() => {
         }
 
         return audioData;
-    }catch(err){
+    } catch (err) {
         throw err;
     }
 }
